9b40c0eb5ba3a72206a4ad1ae50a094d230ae38f
unknown <sysadmin@christinmorton.com>
2024-06-27 07:17:23 -0700

    working backend api app in a production environment...

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', process.env.POSTGRES_DATABASE),
-        user: env('DATABASE_USERNAME', process.env.POSTGRES_USER),
-        password: env('DATABASE_PASSWORD', process.env.POSTGRES_PASSWORD),
-      },
-      debug: false,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', process.env.MYSQL_DATABASE),
+      user: env('DATABASE_USERNAME', process.env.MYSQL_USER),
+      password: env('DATABASE_PASSWORD', process.env.MYSQL_PASSWORD),
     },
-  });
+    debug: false,
+  },
+});
